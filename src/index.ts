9dfa175ae18699a43e88ca19d875892f291b7dff
keonlee <rjsdlwwkd@kaist.ac.kr>
2020-03-09 17:25:36 +0900

    #6

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,21 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "keon",
-  age: 22,
-  gender: "male"
-};
+const keon = new Human("keon", 22, "male");
 
 const sayHi = (person: Human): string => {
   const { name, age, gender } = person;
   return `Hello ${name}, you are ${age}, you are a ${gender}!`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(keon));
 
 export {};
